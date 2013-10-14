ed7f46743745ba9ff35f59dace3d78904e305e81
Joe Werle <joseph.werle@gmail.com>
2013-10-14 16:29:06 -0400

    bumped version

--- src/sphia.h
+++ src/sphia.h
@@ -16,7 +16,7 @@
 #include <sp.h>
 
 
-#define SPHIA_VERSION "0.0.4"
+#define SPHIA_VERSION "0.0.5"
 
 #define SPHIA_DB_FOREACH(_k, _v, _db)                      \
   char *_k = NULL, *_v = NULL;                             \
