fd95787a6478df10581c91547f2970def2bba5ca
Stephen Mathieson <smath23@gmail.com>
2013-11-02 12:09:33 -0400

    refactor test structure
    - compile each test into its own binary file and execute it
    - create a simple `sphia-test.h` which holds setup/teardown stuff
    
    makefile is a bit ugly (usage of `$(basename)` specifically), so i'm up for a discussion here :)

---
+++ test/sphia-test.h
@@ -0,0 +1,25 @@
+
+#ifndef __SPHIA_TEST_H__
+#define __SPHIA_TEST_H__ 1
+
+#include <assert.h>
+#include "../src/sphia.h"
+
+// global sphia pointer
+sphia_t *sphia;
+
+static void
+test_setup() {
+  sphia = sphia_new("./test-db");
+  assert(sphia);
+}
+
+static void
+test_teardown() {
+  // empty the db
+  sphia_clear(sphia);
+
+  sphia_free(sphia);
+}
+
+#endif
