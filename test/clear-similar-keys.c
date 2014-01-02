cc7b316872403c586627bd909bb596801e4a67cf
Stephen Mathieson <smath23@gmail.com>
2014-01-02 07:20:19 -0500

    Update clear test to use keys longer than 8 chars

--- test/clear-similar-keys.c
+++ test/clear-similar-keys.c
@@ -6,7 +6,9 @@ static void
 test_clear_similar_keys() {
   sphia_set(sphia, "key-1", "hello world");
   sphia_set(sphia, "key-10", "hello world");
-  assert(2 == sphia_count(sphia));
+  sphia_set(sphia, "00000000", "hello world");
+  sphia_set(sphia, "000000000", "hello world");
+  assert(4 == sphia_count(sphia));
   assert(0 == sphia_clear(sphia));
   assert(0 == sphia_count(sphia));
 }
